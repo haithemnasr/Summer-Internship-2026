3fb1daefaa34b50d185c04d952c51ff3261868fe
HaithemGitHub <haithemnasr09@gmail.com>
2026-06-24 14:32:00 +0200

    Third upload

--- Multi-Plateform Interactive Hash Table Visualizer/Step 1/src/main/java/XHashtable.java
+++ Multi-Plateform Interactive Hash Table Visualizer/Step 1/src/main/java/XHashtable.java
@@ -1,33 +1,61 @@
-import java.util.LinkedList;
-
 public class XHashtable {
-    LinkedList<String>[] table;
+    Node[] table;
     int size;
     public XHashtable(int size){
         this.size=size;
-        table = new LinkedList[size];
-        for(int i=0; i<size; i++){
-            table[i]= new LinkedList<>();
+        table = new Node[size];
+    }
+    int hash(String key) {
+        int hash = 0;
+        for (int i = 0; i < key.length(); i++) {
+            hash = (hash * 31 + key.charAt(i)) % size;
         }
+        return hash;
     }
-    int hash(String key){
-        return (key.hashCode() & 0x7fffffff) % size;
-    }
-    public void add(String key){
+    public void add(String key) {
         int index = hash(key);
-        if (!table[index].contains(key)) {
-            table[index].add(key);
+        // check if key already exists
+        Node current = table[index];
+        while (current != null) {
+            if (current.key.equals(key)){
+                return;
+            }
+            current = current.next;
         }
+        Node newNode = new Node(key);
+        newNode.next = table[index];
+        table[index] = newNode;
     }
     public boolean delete(String key){
         int index = hash(key);
-        return table[index].remove(key);
+        Node current = table[index];
+        Node prev = null;
+        while (current != null){
+            if (current.key.equals(key)){
+                if (prev == null){
+                    table[index] = current.next;
+                }else{
+                    prev.next = current.next;
+                }
+                return true;
+            }
+            prev = current;
+            current = current.next;
+        }
+        return false;
     }
     public boolean find(String key){
         int index = hash(key);
-        return table[index].contains(key);
+        Node current = table[index];
+        while (current != null){
+            if (current.key.equals(key)){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
     }
-    public LinkedList<String>[] getTable() {
+    public Node[] getTable() {
         return table;
     }
 }
--- Multi-Plateform Interactive Hash Table Visualizer/Step 1/src/main/java/XHashtableDrawer.java
+++ Multi-Plateform Interactive Hash Table Visualizer/Step 1/src/main/java/XHashtableDrawer.java
@@ -1,15 +1,15 @@
-import java.util.LinkedList;
 import net.thevpc.nuts.io.NOut;
 import net.thevpc.nuts.text.NMsg;
 public class XHashtableDrawer {
-
     public void draw(XHashtable hashTable) {
-        LinkedList<String>[] table = hashTable.getTable();
+        Node[] table = hashTable.getTable();
         for (int i = 0; i < table.length; i++) {
             StringBuilder str = new StringBuilder();
             str.append(i).append(" -> ");
-            for (String value : table[i]) {
-                str.append(value).append(" ");
+            Node current = table[i];
+            while (current != null) {
+                str.append(current.key).append(" ");
+                current = current.next;
             }
             NOut.println(NMsg.ofC("##:5:%s##", str));
         }
